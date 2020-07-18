42e8612642506b8010f1bbdec77fcd3ed7167736
Rafael Abrantes <rafael.vieira.abranttes@gmail.com>
2020-07-17 22:10:47 -0300

    Tabela padrão para testes

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,55 @@
+import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 main() => runApp(ExpensesApp());
 
-
 class ExpensesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MyHomePage()
-    );
+    return MaterialApp(home: MyHomePage());
   }
 }
 
 class MyHomePage extends StatelessWidget {
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo Tênis de Corrida',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      title: 'Conta de luz',
+      value: 211.30,
+      id: 't2',
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Center(child: Text('Despesas Pesoais')),
-      ),
-      body: Column(
-        children: <Widget>[
-          Container(
-            width: double.infinity,
-            child: Card(
-              child: Text('Gráfico'),
-              elevation: 5,
+        appBar: AppBar(
+          title: Center(child: Text('Despesas Pesoais')),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              child: Card(
+                child: Text('Gráfico'),
+                elevation: 5,
+              ),
             ),
-          ),
-          Card(
-            child: Text('Lita de Transações')
-          )
-        ],
-      )
-    );
+            Column(
+              children: _transactions.map((tr) {
+                return Card(
+                  child: Text(tr.title),
+                );
+              }).toList(),
+            ),
+          ],
+        ));
   }
 }
